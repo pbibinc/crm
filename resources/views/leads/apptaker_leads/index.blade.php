0a06ce1c2ee3efa927ddcd0c875d86758dcacc4f
maechael108 <maechael@pbibinc@gmail.com>
2023-10-23 10:39:47 -0700

    commit changes

--- resources/views/leads/apptaker_leads/index.blade.php
+++ resources/views/leads/apptaker_leads/index.blade.php
@@ -11,7 +11,7 @@
                                 <div class="d-flex">
                                     <div class="flex-grow-1">
                                         <p class="text-truncate font-size-14 mb-2">Total Appointed Leads</p>
-                                        <h4 class="mb-2">{{ count($dataCount) ?? 0 }}</h4>
+                                        <h4 class="mb-2">{{ isset($dataCount) ? count($dataCount) : 0 }}</h4>
                                         <p class="text-muted mb-0"><span class="text-success fw-bold font-size-12 me-2"><i
                                                     class="ri-arrow-right-up-line me-1 align-middle"></i>9.23%</span>from
                                             previous period</p>
