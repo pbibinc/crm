8c1cd36c1da7e93de1f6dda6b5854c822d9b8069
maechael108 <maechael@pbibinc@gmail.com>
2023-11-06 15:05:46 -0800

    changes for cors configuration

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => [
+        env('CORS_DEV_ORIGIN', 'http://localhost:3000'),
+        env('CORS_PROD_ORIGIN', 'https://crm.pbibinc.com')
+    ],
 
     'allowed_origins_patterns' => [],
 
