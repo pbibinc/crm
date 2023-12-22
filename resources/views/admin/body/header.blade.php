366a7b54004c19f1f386dd508abfb79ed222741a
maechael108 <maechael@pbibinc@gmail.com>
2023-12-23 07:45:48 +0800

    implementation of notifacation

--- resources/views/admin/body/header.blade.php
+++ resources/views/admin/body/header.blade.php
@@ -41,6 +41,11 @@
                     <i class="ri-fullscreen-line"></i>
                 </button>
             </div>
+            <div class="dropdown d-none d-lg-inline-block ms-1">
+                <button type="button" class="btn header-item noti-icon waves-effect" data-toggle="fullscreen">
+                    <i class="ri-fullscreen-line"></i>
+                </button>
+            </div>
 
             @php
                 $id = auth()->user()->id;
