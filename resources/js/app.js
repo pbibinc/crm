c74f22ba7a75437b4d39c251154ea9d23d0b66cf
maechael108 <maechael@pbibinc@gmail.com>
2024-01-27 05:27:30 +0800

    commit changes in app etc

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 require("./bootstrap");
 
 import Alpine from "alpinejs";
-
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
 import * as Ladda from "ladda";
 
 // import $ from "jquery";
@@ -12,7 +13,7 @@ import * as Ladda from "ladda";
 
 // window.jQuery = $;
 // window.$ = $;
-
+window.Pusher = Pusher;
 window.Alpine = Alpine;
 
 Alpine.start();
@@ -20,15 +21,29 @@ Alpine.start();
 // Find the notification element
 // const notificationElement = document.getElementById('notification');
 
-// Listen for UserSessionChange events on the notifications channel
-Echo.channel("notifications").listen("UserSessionChange", (e) => {
-  const notificationElement = document.getElementById("notification");
-  console.log("UserSessionChange event received:", e);
-  debugger;
-  // Update the notification element text and classes based on the event data
-  notificationElement.innerText = e.message;
-  notificationElement.classList.remove("invisible");
-  notificationElement.classList.remove("alert-success");
-  notificationElement.classList.remove("alert-danger");
-  notificationElement.classList.add("alert-" + e.type);
+window.Echo = new Echo({
+  broadcaster: "pusher",
+  key: process.env.MIX_PUSHER_APP_KEY,
+  cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+  forceTLS: true,
+  withCredentials: true,
+  auth: {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  },
+  // authEndpoint: "/broadcasting/auth",
 });
+
+// Listen for UserSessionChange events on the notifications channel
+// Echo.channel("notifications").listen("UserSessionChange", (e) => {
+//   const notificationElement = document.getElementById("notification");
+//   console.log("UserSessionChange event received:", e);
+//   debugger;
+//   // Update the notification element text and classes based on the event data
+//   notificationElement.innerText = e.message;
+//   notificationElement.classList.remove("invisible");
+//   notificationElement.classList.remove("alert-success");
+//   notificationElement.classList.remove("alert-danger");
+//   notificationElement.classList.add("alert-" + e.type);
+// });
