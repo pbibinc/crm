74d5aa778fdd56e97a0a99b18178e4374273966d
maechael108 <maechael@pbibinc@gmail.com>
2024-08-16 03:17:14 +0800

    push general liabilitiy previous use effect

--- product-form/src/data/general-liabilities-previous-data.jsx
+++ product-form/src/data/general-liabilities-previous-data.jsx
@@ -19,7 +19,7 @@ const GeneralLiabilitiPreviousData = () => {
             }
         };
         fetchGeneralLiability();
-    }, [getLeadData?.data?.activityId]);
+    }, [getLeadData?.data?.activityId, lead]);
     return { generalLiabilityPreviousData };
 };
 
