229ff14b62c64c0056e4a065e243e9f632f65e01
maechael108 <maechael@pbibinc@gmail.com>
2024-11-27 00:24:08 +0800

    c

--- config/sanctum.php
+++ config/sanctum.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     | Stateful Domains
     |--------------------------------------------------------------------------
-    |
+
     | Requests from the following domains / hosts will receive stateful API
     | authentication cookies. Typically, these should include your local
     | and production domains which access your API via a frontend SPA.
